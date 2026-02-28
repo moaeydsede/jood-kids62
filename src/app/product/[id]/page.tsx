f4690c59a3a2052a7691bb1aeb8ca0d205dcc560
moaeydsede <moaeydsede98@gmail.com>
2026-02-28 15:55:53 +0200

    Delete src directory

--- src/app/product/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import ProductDetails from "@/components/ProductDetails";
-
-export default function ProductPage({ params }: { params: { id: string } }) {
-  return <ProductDetails productId={params.id} />;
-}
--- src/lib/security.ts
+++
@@ -1,5 +0,0 @@
-export const ADMIN_UID = "31ZskJ12hdNhy5D5lwP6dPB5Kw92";
-
-export function isAdminUid(uid: string) {
-  return uid === ADMIN_UID;
-}
